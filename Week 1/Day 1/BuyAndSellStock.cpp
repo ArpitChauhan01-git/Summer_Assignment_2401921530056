574581044beb6893b6a8d3f3d0f1fb952c6c9add
Arpit Chauhan <arpitchauhan5734@gmail.com>
2026-06-01 21:35:29 +0530

    Added Week-1 Day-1 Leetcode Solutions

---
+++ Week 1/Day 1/BuyAndSellStock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0, buyStock = prices[0];
+        int n = prices.size();
+        for(int i=0; i<n; i++){
+            if( prices[i] < buyStock){
+                buyStock = prices[i];
+            }
+            profit = max(profit, prices[i]-buyStock);
+        }
+        return profit;
+    }
+};
---
+++ Week 1/Day 1/RemoveDuplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size(),k=1,i=1,flag=nums[0];
+        while(i<n){
+            if(nums[i] != nums[i-1] && nums[i] != flag){
+                flag=nums[i];
+                swap(nums[i], nums[k]);
+                k++;
+            }
+            i++;
+        }
+        return k;
+    }
+};
